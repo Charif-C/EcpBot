3144c39233f9baf55a03ea979e2e7d76ca2d47db
yaay13 <yay13@live.fr>
2017-11-09 21:32:44 +0100

    Getters defined

--- BrotherTongueBot/BrotherTongueBot/MainClass.py
+++ BrotherTongueBot/BrotherTongueBot/MainClass.py
@@ -17,45 +17,25 @@ def main():
             user_message=input()
 
         rep=Response(ai._get_json_response(user_message))
-        
-        result = rep.result
-        action = rep.action
-        intent = rep.intent
-
-        if intent == "AuRevoir":
+        try:
+            _result = rep.result
+            _action = rep.result.action
+            _intent = rep.result.metadata.intentName
+        except:
+            pass
+        if _intent == "AuRevoir":
             print("Au revoir !")
             break
 
-        actionIncomplete = rep.actionIncomplete
-        contexts = rep.contexts
+        actionIncomplete = rep.result.actionIncomplete
+        contexts = rep.result.contexts
 
         if len(contexts)>0 :
             context = contexts[0]
         else:
             context=None
 
-        print(u"< %s" % rep.speech)
-
-        if action is not None:
-            if action == u"send_message":
-                parameters = rep.parameters
-
-                text = rep.text
-                message_type = rep.message_type
-                parent = rep.parent
-
-                print (
-                    'text: %s, message_type: %s, parent: %s' %
-                    (
-                        text if text else "null",
-                        message_type if message_type else "null",
-                        parent if parent else "null"
-                    )
-                )
-
-                if not actionIncomplete:
-                    print(u"...Sending Message...")
-                    break
+        print(u"< %s" % rep.result.fulfillment.speech)
 
 
 if __name__ == '__main__':
--- BrotherTongueBot/BrotherTongueBot/Response.py
+++ BrotherTongueBot/BrotherTongueBot/Response.py
@@ -2,14 +2,49 @@ class Response:
     """description of class"""
     def __init__(self, response):
         """Constructeur de notre classe"""
+        self.response=response
+        self.id =  response['id']
+        self.timestamp =  response['timestamp']
+        self.lang =  response['lang']
 
-        self.result =  response['result']
-        self.action = response['result'].get('action')
-        self.intent = response['result']["metadata"].get('intentName')
-        self.actionIncomplete = response['result'].get('actionIncomplete', False)
-        self.contexts = response['result'].get('contexts')    
-        self.parameters = self.result['parameters']
-        self.text = self.result['parameters'].get('text')
-        self.message_type = self.result['parameters'].get('message_type')
-        self.parent = self.result['parameters'].get('parent')
-        self.speech = response['result']['fulfillment']['speech']
+        #self.result =  Result(response['result'])
+        #self.alternateResult =  response.get('alternateResult'
+    @property
+    def result(self):
+        return Result(self.response['result'])
+    @property
+    def status(self):
+        return Status(self.response['status'])
+
+class Result:
+    def __init__(self,result):
+        self.result= result
+        self.action=result['action']
+        self.source=result['source']
+        self.resolvedQuery=result['resolvedQuery']
+        self.actionIncomplete=result['actionIncomplete']
+        self.parameters=result['parameters']
+        self.score=result['score']
+        self.contexts=result['contexts']
+    @property
+    def metadata(self):
+        return Metadata(self.result['metadata'])
+    @property
+    def fulfillment(self):
+        return Fulfillment(self.result['fulfillment'])
+
+class Metadata:
+    def __init__(self,metadata):
+        self.intentId=metadata['intentId']
+        self.webhookUsed=metadata['webhookUsed']
+        self.webhookForSlotFillingUsed=metadata['webhookForSlotFillingUsed']
+        self.intentName=metadata['intentName']
+class Fulfillment:
+    def __init__(self,fulfillment):
+        self.speech=fulfillment['speech']
+        self.messages=fulfillment['messages']
+class Status:
+    def __init__(self,status):
+        self.code=status['code']
+        self.errorType=status['errorType']
+
